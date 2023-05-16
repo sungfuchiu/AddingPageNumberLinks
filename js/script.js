66e415509c3ab42aca833261bac7c4de3e47b745
sungfuchiu <sungfuchiu@gmail.com>
2023-05-15 22:03:36 -0700

    Use a template to rewrite list item

--- js/script.js
+++ js/script.js
@@ -3,6 +3,14 @@ const pageSize = 10;
 const numberOfPages = Math.ceil(users.length/pageSize);
 const pageArray = [...Array(numberOfPages).keys()].map(i => i+1);
 const contactListDiv = document.getElementById("contact-list");
+let contactTemplate = `<div class="contact-details">
+    <img class="avatar" src="@imageURL">
+    <h3>@name</h3>
+    <span class="email">@emailUsername@example.com</span>
+</div>
+<div class="joined-details">
+       <span class="date">Joined @joinDate</span>
+</div>`;
 
 pageArray.forEach(i => {
     const pageInput = document.createElement("input");
@@ -15,35 +23,14 @@ loadAPage();
 function loadAPage(pageNumber = 1){
     const startIndex = (pageNumber - 1) * pageSize;
     [...Array(pageSize).keys()].map(i => i+startIndex).forEach(i => {
+        let contactHTML = contactTemplate.replace("@imageURL", users[i].image);
+        contactHTML = contactHTML.replace("@name", users[i].name);
+        contactHTML = contactHTML.replace("@joinDate", users[i].joined);
+        contactHTML = contactHTML.replace("@emailUsername", users[i].name.split(" ").join("."));
+        
         const userLi = document.createElement("li");
         userLi.className = "contact-item cf";
-
-        const contactDetailDiv = document.createElement("div");
-        contactDetailDiv.className = "contact-details";
-
-        const contactDetailImg = document.createElement("img");
-        contactDetailImg.className = "avatar";
-        contactDetailImg.src = users[i].image;
-        contactDetailDiv.appendChild(contactDetailImg);
-
-        const contactDetailH3 = document.createElement("h3");
-        contactDetailH3.textContent = users[i].name;
-        contactDetailDiv.appendChild(contactDetailH3);
-
-        const contactDetailSpan = document.createElement("span");
-        contactDetailSpan.className = "email";
-        contactDetailSpan.textContent = "@example.com";
-        contactDetailDiv.appendChild(contactDetailSpan);
-
-        userLi.appendChild(contactDetailDiv);
-
-        const joinedDetailDiv = document.createElement("div");
-        joinedDetailDiv.className = "joined-details";
-        const joinDate = document.createElement("span");
-        joinDate.textContent = `Joined ${users[i].joined}`;
-        joinedDetailDiv.appendChild(joinDate);
-
-        userLi.appendChild(joinedDetailDiv);
+        userLi.innerHTML = contactHTML;
 
         contactListDiv.appendChild(userLi);
     });
