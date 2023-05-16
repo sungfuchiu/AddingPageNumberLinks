2d6982b7f09868af75bfbd4c8a29870d6a681239
sungfuchiu <sungfuchiu@gmail.com>
2023-05-15 23:17:20 -0700

    Finish all the functions
    1. Make contact number changeable
    2. Add event on page number button for loading contacts

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const pageSize = 10;
 const numberOfPages = Math.ceil(users.length/pageSize);
 const pageArray = [...Array(numberOfPages).keys()].map(i => i+1);
 const contactListDiv = document.getElementById("contact-list");
+const contactNumber = document.getElementById("contact-number");
 let contactTemplate = `<div class="contact-details">
     <img class="avatar" src="@imageURL">
     <h3>@name</h3>
@@ -12,15 +13,29 @@ let contactTemplate = `<div class="contact-details">
        <span class="date">Joined @joinDate</span>
 </div>`;
 
+contactNumber.innerText = users.length;
+loadAPage();
 pageArray.forEach(i => {
     const pageInput = document.createElement("input");
     pageInput.type = "button";
     pageInput.value = i;
+    pageInput.onclick = () => loadAPage(i);
+    pageInput.addEventListener('click', (event) => clickPageButton(event));
+    pageInput.disabled = (i === 1);
     paginationDiv.appendChild(pageInput);
 });
-loadAPage();
+function clickPageButton(event){
+    Array.from(paginationDiv.children).forEach(pageInput => {
+        pageInput.disabled = false;
+    })
+    const input = event.target;
+    const value = input.value;
+    input.disabled = true;
+    loadAPage(value);
+}
 
 function loadAPage(pageNumber = 1){
+    contactListDiv.innerHTML = "";
     const startIndex = (pageNumber - 1) * pageSize;
     [...Array(pageSize).keys()].map(i => i+startIndex).forEach(i => {
         let contactHTML = contactTemplate.replace("@imageURL", users[i].image);
