8221a65d557a45617f1e950356df30c726a91416
sungfuchiu <sungfuchiu@gmail.com>
2023-05-16 12:06:40 -0700

    Trivial modifications
    1. modify placeholder to make them more obvious
    2. remove an useless line
    3. resolve the bug that for loop would exceed the length of the array

--- js/script.js
+++ js/script.js
@@ -4,13 +4,14 @@ const numberOfPages = Math.ceil(users.length/pageSize);
 const pageArray = [...Array(numberOfPages).keys()].map(i => i+1);
 const contactListDiv = document.getElementById("contact-list");
 const contactNumber = document.getElementById("contact-number");
-let contactTemplate = `<div class="contact-details">
-    <img class="avatar" src="@imageURL">
-    <h3>@name</h3>
-    <span class="email">@emailUsername@example.com</span>
+let contactTemplate = `
+<div class="contact-details">
+    <img class="avatar" src="{@imageURL}">
+    <h3>{@name}</h3>
+    <span class="email">{@emailUsername}@example.com</span>
 </div>
 <div class="joined-details">
-       <span class="date">Joined @joinDate</span>
+       <span class="date">Joined {@joinDate}</span>
 </div>`;
 
 contactNumber.innerText = users.length;
@@ -19,11 +20,11 @@ pageArray.forEach(i => {
     const pageInput = document.createElement("input");
     pageInput.type = "button";
     pageInput.value = i;
-    pageInput.onclick = () => loadAPage(i);
     pageInput.addEventListener('click', (event) => clickPageButton(event));
     pageInput.disabled = (i === 1);
     paginationDiv.appendChild(pageInput);
 });
+
 function clickPageButton(event){
     Array.from(paginationDiv.children).forEach(pageInput => {
         pageInput.disabled = false;
@@ -38,10 +39,13 @@ function loadAPage(pageNumber = 1){
     contactListDiv.innerHTML = "";
     const startIndex = (pageNumber - 1) * pageSize;
     [...Array(pageSize).keys()].map(i => i+startIndex).forEach(i => {
-        let contactHTML = contactTemplate.replace("@imageURL", users[i].image);
-        contactHTML = contactHTML.replace("@name", users[i].name);
-        contactHTML = contactHTML.replace("@joinDate", users[i].joined);
-        contactHTML = contactHTML.replace("@emailUsername", users[i].name.split(" ").join("."));
+        if(i >= users.length){
+            return;
+        }
+        let contactHTML = contactTemplate.replace("{@imageURL}", users[i].image);
+        contactHTML = contactHTML.replace("{@name}", users[i].name);
+        contactHTML = contactHTML.replace("{@joinDate}", users[i].joined);
+        contactHTML = contactHTML.replace("{@emailUsername}", users[i].name.split(" ").join("."));
         
         const userLi = document.createElement("li");
         userLi.className = "contact-item cf";
